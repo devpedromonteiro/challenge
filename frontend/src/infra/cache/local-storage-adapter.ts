22732c75fe98a2418631b7f4e8e1a2def7de7ea6
João Pedro Monteiro Pereira <devpedromonteiro@gmail.com>
2025-11-05 22:35:43 -0300

    feat(cache): OP-3 implement local storage adapter for caching
    This commit introduces a LocalStorageAdapter class that
    implements caching functionalities using the browser's
    localStorage. The adapter provides methods to set and
    retrieve data, allowing for persistence of application
    state across sessions.
    
    The primary reason for this implementation is to enhance
    performance by reducing redundant network requests and
    improving the user experience through faster data access.
    By caching responses, we aim to provide immediate data
    availability, especially in cases where network
    connectivity might be slow or unpredictable.
    
    Additionally, a new AxiosHttpClient class is added to
    handle HTTP requests using Axios. This class abstracts
    the HTTP request logic, making it easier to manage
    responses and errors in a unified way. Both of these
    changes lay the foundation for more efficient and
    responsive frontend development.

---
+++ frontend/src/infra/cache/local-storage-adapter.ts
@@ -0,0 +1,17 @@
+import type { GetStorage, SetStorage } from '@/data/protocols/cache'
+
+export class LocalStorageAdapter implements SetStorage, GetStorage {
+  set(key: string, value: any): void {
+    if (value) {
+      localStorage.setItem(key, JSON.stringify(value))
+    } else {
+      localStorage.removeItem(key)
+    }
+  }
+
+  get(key: string): any {
+    const item = localStorage.getItem(key)
+    return item ? JSON.parse(item) : null
+  }
+}
+
